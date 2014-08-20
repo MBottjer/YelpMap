f1b7602f10ecc82eeee9187bc944c5193fa330fb
Michael Bottjer <michaelbottjer@hotmail.com>
2014-08-20 14:12:54 +0100

    add data endpoint

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,10 @@
 class MapController < ApplicationController
   def index
+
+  end
+
+  def data
+  	@@data = File.read("#{Rails.root}/app/assets/javascripts/yelp_edinburgh.json")
+  	render :json => @@data
   end
 end
